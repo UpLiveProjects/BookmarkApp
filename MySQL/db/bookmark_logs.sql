a6a92912ffa2a089bf2f157c296f75600f4cece8
UpLiveProjects <110183671+UpLiveProjects@users.noreply.github.com>
2023-01-23 21:31:41 +0200

    Delete MySQL directory

--- MySQL/db/bookmark_logs.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE bookmark_logs (
-  id int(11) NOT NULL,
-  time_stamp datetime NOT NULL,
-  user_name text NOT NULL,
-  task text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
--- MySQL/db/bookmark_users.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE bookmark_users (
-  id int(11) NOT NULL,
-  user_name text NOT NULL,
-  password text NOT NULL,
-  level text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
